6d4f114effe10bbddf70158617cb92fabe70d261
Ganesh Nimbalkar <142650963+Ganesh-Nimbalkar@users.noreply.github.com>
2024-09-05 15:04:40 +0530

    Add files via upload

---
+++ management House-Backend/Java-Spring-boot-Project/README.md
@@ -0,0 +1,4 @@
+# Java-Spring-boot-Project
+A robust Java Spring Boot e-commerce website with secure authentication, RESTful APIs, and MySQL integration."
+
+This repository contains a fully functional e-commerce website built using Java and Spring Boot. The application features a robust backend with RESTful APIs, secure user authentication, product management, and order processing. It also integrates with a MySQL database for data persistence and utilizes Spring Security for user authentication and authorization. The project is designed to be scalable and maintainable, with a focus on clean code and best practices.
---
+++ management House-Backend/src/main/java/com/Mhouse/Service/ProductsService.java
@@ -0,0 +1,77 @@
+// ProductsService.java
+package com.Mhouse.Service;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.Mhouse.Model.ProductsModel;
+import com.Mhouse.Repository.ProductsRepository;
+
+@Service
+public class ProductsService {
+
+    @Autowired
+    ProductsRepository apr;
+
+    public String addProduct(String name, String brand, int price, MultipartFile image) {        ProductsModel apm = new ProductsModel();
+        apm.setName(name);
+        apm.setBrand(brand);
+        apm.setPrice(price);
+        try {
+            apm.setImage(Base64.getEncoder().encodeToString(image.getBytes()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        apm.setDatetime(new Date());
+        apr.save(apm);
+        return "1 product has been added successfully";
+    }
+
+    public List<ProductsModel> getAllProductsWithoutImage() {
+        List<ProductsModel> products = apr.findAll();
+        for (ProductsModel product : products) {
+            // Set the image field to null to exclude it from the response
+            product.setImage(null);
+        }
+        return products;
+    }
+
+
+    public void deleteProduct(Long productId) {
+        apr.deleteById(productId);
+    }
+
+    public ProductsModel getProductById(Long productId) {
+        return apr.findById(productId).orElse(null);
+    }
+    
+    
+    
+
+    public String updateProduct(Long productId, String name, String brand, int price, MultipartFile image) {
+        ProductsModel apm = apr.findById(productId)
+                .orElseThrow(() -> new EntityNotFoundException("Product not found with id: " + productId));
+        apm.setName(name);
+        apm.setBrand(brand);
+        apm.setPrice(price);
+        apm.setDatetime(new Date());
+        try {
+            apm.setImage(Base64.getEncoder().encodeToString(image.getBytes()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        apr.save(apm);
+        return "1 product has been updated successfully";
+    }
+
+    public List<ProductsModel> findByNameOrBrand(String data) {
+        return apr.findByNameOrBrand(data, data);
+    }
+}
